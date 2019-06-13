ef12ff1d58d51a656ac13ebdba13920d1c4611ae
Jonathan Clarke <jonathan@vayu.com.au>
2019-06-13 12:31:26 +0545

    Alterations for multi-pipeline support

--- bin/check_build.rb
+++ bin/check_build.rb
@@ -13,7 +13,7 @@ cmd_str = "gcloud builds list --limit=10000 --page-size=10000 >> builds.log"
 
 contents = File.read('./builds.log')
 contents.split("\n").each do |line|
-  if line.include? "#{ app }@#{ branch }"     
+  if line.include? "#{ app }@#{ branch }"
     `echo "#{ line }" >> builds-app.log`
   end
 end
@@ -26,44 +26,44 @@ branch_found = false
 complete_build = false
 result = false
 
-while branch_found == false
-  contents = File.read('./builds-app.log')
-  lines = contents.split("\n")
-  lines[0...3].each do |line|
-    record = line.split(" ")
-    record_result =  `gcloud builds describe #{ record[0] }`
-    puts "checking #{ line }"
-    if record_result.include?(sha)
-      puts "Found the build"
-      puts record_result
-      build_id = record[0]
-      branch_found = true
-      break
-    end
+contents = File.read('./builds-app.log')
+lines = contents.split("\n")
+lines[0...3].each do |line|
+  record = line.split(" ")
+  record_result =  `gcloud builds describe #{ record[0] }`
+  puts "checking #{ line }"
+  if record_result.include?(sha)
+    puts "Found the build"
+    build_id = record[0]
+    branch_found = true
   end
 end
 
 puts "Now we know the build id: #{ build_id } let's keep asking google if it's passed"
-
-while complete_build == false
-  record_result =  `gcloud builds describe #{ build_id }`
-  if record_result.include?(sha)    
-    if record_result.include? "SUCCESS"
-      puts "\n#{ commit } was a SUCCESS"
-      result = 0
-      complete_build = true
-      exit(result)
-    elsif record_result.include? "FAILURE"
-      puts "\n#{ commit } was a FAILURE"
-      result = 1
-      complete_build = true
-      exit(result)
-    else
-      print "."
-      complete_build = false
+if branch_found == true
+  
+  while complete_build == false
+    record_result =  `gcloud builds describe #{ build_id }`
+    if record_result.include?(sha)    
+      if record_result.include? "SUCCESS"
+        puts "\n#{ commit } was a SUCCESS"
+        result = 0
+        complete_build = true
+        exit(result)
+      elsif record_result.include? "FAILURE"
+        puts "\n#{ commit } was a FAILURE"
+        result = 1
+        complete_build = true
+        exit(result)
+      else
+        print "."
+        complete_build = false
+      end
     end
   end
+else
+  puts "\n#{ commit } was not found in GOOGLE CLOUD BUILD"
+  exit(1)
 end
-
 exit(result)
 
